1ab80d5383e231c696bcf11d84d842cd568b9e36
Guimbreon <103893299+Guimbreon@users.noreply.github.com>
2022-12-16 11:18:41 +0000

    Add files via upload

---
+++ Ex_7.py
@@ -0,0 +1,43 @@
+#File I/O
+#1
+print("PART 01- Samples and coordinates")
+file1=[]
+filedic={}
+with open("/home/guimbreon/Desktop/FACULDADE/Programação/to_use_01.txt", "r") as gff:
+    for lines in gff:
+        if lines.startswith("Sample"):
+            continue  
+        data1=lines.split() 
+        print(data1)
+        key,value=data1[0],str(data1[1])+" "+str(data1[1])
+        filedic[key]=value
+    filedic["Var"]="43.13 6.25"
+    filedic["Tun"]="36.95 8.85"
+    print(filedic)
+with open("/home/guimbreon/Desktop/FACULDADE/Programação/to_use_01_new.txt", "w") as gff:
+    for key in filedic: 
+        gff.write("\n")  
+        gff.write(key)
+        gff.write("\n") 
+        gff.write(filedic[key])
+#2
+print("02 - Basic data filtering")
+filedic2={}
+with open("/home/guimbreon/Desktop/FACULDADE/Programação/to_use_02.txt", "r") as gff:
+    for lines in gff:
+        if lines.startswith("Sample"):
+            continue
+        degrees=(72-32)/1.8
+        data2=lines.split()
+        key,value=data2[0],str(data2[1:14])
+        filedic2[key]=value
+    print(filedic2)
+with open("/home/guimbreon/Desktop/FACULDADE/Programação/to_use_02_new.txt", "w") as gff:
+    for key in filedic2:
+        gff.write(key)
+        gff.write("\n")
+        gff.write(filedic2[key])
+        if key=="Bio3":
+            #Write here new code?
+            print(key)
+        gff.write("\n") 
---
+++ Ex_8.py
@@ -0,0 +1,33 @@
+print("Exercicses 08")
+print("01 - DNA Sequences")
+def reverse(sequence):
+    if "U" in sequence:
+        print("U inputed a RNA")
+    else:
+        sequence=sequence[::-1]
+        sequence=sequence.replace("A","t")
+        sequence=sequence.replace("T","a")
+        sequence=sequence.replace("C","g")
+        sequence=sequence.replace("G","c")
+        #sequence=sequence.replace("A","t").replace("T","a").replace("C","g").replace("G","c")
+        # should i use the line above or the other 4?
+        print(sequence.upper())
+def codon(sequence_codon):
+    new=""
+    for letter in sequence_codon:
+        new+=letter
+        if len(new)==5:
+            print(new)
+            if "ATG" in new:
+                print("SIM")
+reverse(sequence=input("Which sequence do you want to analyze:\n>>"))
+codon(sequence_codon=input("Which sequence do you want to analyze:\n>>"))
+with open("/home/guimbreon/Desktop/Programação/Assignment/sequence_ena.fasta", "r") as gff:
+    def fasta(gff):
+        keys,values=[],[]
+        for lines in gff:
+            if lines.startswith(">"):
+                lines=lines.replace(">","")
+                key=lines
+        print(*keys) 
+#fasta(gff)
